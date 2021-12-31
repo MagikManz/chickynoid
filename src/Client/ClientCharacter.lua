d4cedc355e5518d82b80904c31f02422f7a17083
Dan <magikaman12@gmail.com>
2021-12-31 20:07:23 +0200

    Switch to GetDescendants to allow for proper collision checks

--- src/Client/ClientCharacter.lua
+++ src/Client/ClientCharacter.lua
@@ -36,7 +36,7 @@ function ClientCharacter.new(player: Player, position: Vector3)
     }, ClientCharacter)
 
     self._simulation.pos = position
-    self._simulation.whiteList = { workspace.GameArea, workspace.Terrain }
+    self._simulation.whiteList = { workspace.GameArea:GetDescendants(), workspace.Terrain }
 
     if player == LocalPlayer then
         self:_handleLocalPlayer()
